736b2fda60853f20b2b34bc6d435272db26d05d4
xxx <13865248713@163.com>
2025-03-24 17:15:23 +0800

    主题色提交

--- vite.config.js
+++ vite.config.js
@@ -24,4 +24,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
